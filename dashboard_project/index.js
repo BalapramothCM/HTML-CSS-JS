81cc41d6b35b4916ff33955d00411870f492025d
Bala Pramoth C M <57207212+BALAPRAMOTH@users.noreply.github.com>
2025-02-16 23:04:11 +0530

    responsive update for sidebar and included top icon

--- dashboard_project/index.js
+++ dashboard_project/index.js
@@ -1,9 +1,14 @@
 function showSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = "flex"
+    const uparro = document.querySelector('#top_arro')
+    uparro.style.display = "none"
+
 }
 
 function hideSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = "none"
+    const uparro = document.querySelector('#top_arro')
+    uparro.style.display = "block"
 }
